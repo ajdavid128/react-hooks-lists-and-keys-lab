80969e23272e7becab91652c21d60b437ee7700e
Aaron David <ajdavid128@gmail.com>
2022-11-06 14:57:38 -0600

    commit tree 807ff09eaa478a6d04b3d1fb691d7ef70419f0f4

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      <a key={`#${links[1]}`} href={`#${links[1]}`}>home</a>
+      <a key={`#${links[2]}`} href={`#${links[2]}`}>about</a>
+      <a key={`#${links[3]}`} href={`#${links[3]}`}>projects</a>
+      {/* display an <a> tag for each link here */}
+    </nav>);
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({props}) {
+
+  // console.log(props)
+
+  const {name, about, technologies} = props
+
+  console.log(technologies)
+
+  const techArray = technologies.map((eachTech) => {
+    // console.log(eachTech)
+    return (
+      <span key={eachTech}>{eachTech}</span>
+    )
+  })
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+        {techArray}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
